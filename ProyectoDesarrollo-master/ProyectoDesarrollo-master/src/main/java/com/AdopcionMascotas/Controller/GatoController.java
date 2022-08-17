b588656e52a77679528799cbdecfffc57246f758
lyonc <lyonc@MSI-DIEGO>
2022-08-16 22:22:54 -0600

    Card en el html de leergatos

--- ProyectoDesarrollo-master/ProyectoDesarrollo-master/src/main/java/com/AdopcionMascotas/Controller/GatoController.java
+++ ProyectoDesarrollo-master/ProyectoDesarrollo-master/src/main/java/com/AdopcionMascotas/Controller/GatoController.java
@@ -5,6 +5,7 @@ import com.AdopcionMascotas.Service.IGatoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
