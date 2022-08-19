8778673910c06f85aa58ab71adf300532cf311da
BrayanRL01 <brayan12345303@gmail.com>
2022-08-19 16:52:47 -0600

    Se agregaron las clases para poder crear un Reporte Jasper de comentarios

--- ProyectoDesarrollo-master/ProyectoDesarrollo-master/src/main/java/com/AdopcionMascotas/Controller/ComentarioController.java
+++ ProyectoDesarrollo-master/ProyectoDesarrollo-master/src/main/java/com/AdopcionMascotas/Controller/ComentarioController.java
@@ -2,13 +2,20 @@ package com.AdopcionMascotas.Controller;
 
 import com.AdopcionMascotas.Entity.Comentario;
 import com.AdopcionMascotas.Service.IComentarioService;
+import com.AdopcionMascotas.Service.PerroReportService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ComentarioController {
@@ -41,6 +48,27 @@ public class ComentarioController {
     public String EliminarComentario(Comentario C) {
         ICS.EliminarComentario(C.getID());
         return "redirect:/leercomentarios";
+    }
+    
+        @Autowired
+    private PerroReportService PReportService;
+
+    @GetMapping(path = "/leercomentarios/Comentarios", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(PReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
